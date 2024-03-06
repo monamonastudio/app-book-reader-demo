b1b3194f94feb1b24fc89127098c187c0d830da0
Paulo Pertierra <paulopertierra@gmail.com>
2024-03-06 23:33:29 +0800

    feat: extract components

---
+++ src/components/App/AppContent.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="overflow-auto pt-16 pb-48 font-serif">
+    <slot></slot>
+  </div>
+</template>
---
+++ src/components/App/AppNavBar.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowLeft, faArrowUpFromBracket, faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faArrowLeft, faArrowUpFromBracket, faEllipsisVertical);
+</script>
+<template>
+  <div class="h-16 border-b flex items-center p-4 justify-between fixed max-w-[400px] w-full bg-white">
+    <div>
+      <FontAwesomeIcon class="h-6" icon="fa-solid fa-arrow-left" />
+    </div>
+    <div class="flex items-center gap-4">
+      <FontAwesomeIcon class="h-6" icon="fa-solid fa-arrow-up-from-bracket" />
+      <span class="text-red-600 text-sm">Telecharger</span>
+      <FontAwesomeIcon class="h-6" icon="fa-solid fa-ellipsis-vertical" />
+    </div>
+  </div>
+</template>
